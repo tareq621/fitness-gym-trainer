24e4299caae541a6c5f7cfa9fceccecace197367
tareq621 <mirtareq621@gmail.com>
2022-04-18 14:16:35 +0600

    title name

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -3,6 +3,7 @@ import { Carousel } from 'react-bootstrap';
 import banner1 from '../../../images/banner/treadmill.jpg'
 import banner2 from '../../../images/banner/yoga.jpg'
 import banner3 from '../../../images/banner/hiit.jpg'
+import './Banner.css'
 
 const Banner = () => {
     const [index, setIndex] = useState(0);
@@ -12,43 +13,48 @@ const Banner = () => {
     };
 
     return (
-        <Carousel activeIndex={index} onSelect={handleSelect}>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={banner1}
-                    alt="First slide"
-                />
-                <Carousel.Caption>
-                    <h1>TREADMILL WORKOUT FOR RUNNERS</h1>
-                    <p className='text-light'>Everything to do with the treadmill. From exercising on them for beginners, more experienced runners and those looking to lose weight.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={banner2}
-                    alt="Second slide"
-                />
+        <div className='border-2'>
+            <div className='title'>
+                <h1 className='title-name'>Personal Gym Trainer</h1>
+            </div>
+            <Carousel activeIndex={index} onSelect={handleSelect}>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h1>TREADMILL WORKOUT FOR RUNNERS</h1>
+                        <p className='text-light'>Everything to do with the treadmill. From exercising on them for beginners, more experienced runners and those looking to lose weight.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner2}
+                        alt="Second slide"
+                    />
 
-                <Carousel.Caption>
-                    <h1>YOGA FOR EVERYONE</h1>
-                    <p className='text-light'>Yoga is a mind and body practice. Various styles of yoga combine physical postures, breathing techniques, and meditation or relaxation.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={banner3}
-                    alt="Third slide"
-                />
+                    <Carousel.Caption>
+                        <h1>YOGA FOR EVERYONE</h1>
+                        <p className='text-light'>Yoga is a mind and body practice. Various styles of yoga combine physical postures, breathing techniques, and meditation or relaxation.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner3}
+                        alt="Third slide"
+                    />
 
-                <Carousel.Caption>
-                    <h1>HIIT WORKOUT</h1>
-                    <p className='text-light'>HIIT stands for high-intensity interval training and it's one of the most popular forms of exercise.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel>
+                    <Carousel.Caption>
+                        <h1>HIIT WORKOUT</h1>
+                        <p className='text-light'>HIIT stands for high-intensity interval training and it's one of the most popular forms of exercise.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </div>
     );
 };
 
